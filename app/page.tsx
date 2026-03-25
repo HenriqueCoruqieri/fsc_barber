d105b2dba5e025ff9442d5c6f0ebdce6fb762cbb
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-03-24 23:04:57 -0300

    chore: add git commit message linter and husky setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./_components/ui/button"
+
 const Home = () => {
   return (
     <div className="h-screen w-screen bg-neutral-700 text-center">
       <h1 className="text-4xl font-extrabold text-emerald-500">
         Teste TailwindCSS
       </h1>
+      <Button>Teste Shadcn</Button>
     </div>
   )
 }
