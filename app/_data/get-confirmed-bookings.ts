9e13368d5688d2c72dcd57931247016da3895b08
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-04-25 14:41:23 -0300

    refactor: add data functions to get bookings

---
+++ app/_data/get-confirmed-bookings.ts
@@ -0,0 +1,33 @@
+"use server"
+
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
+import { db } from "../_lib/prisma"
+import { User } from "@prisma/client"
+
+export const getConfirmedBookings = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (!session?.user) {
+    return []
+  }
+
+  return db.booking.findMany({
+    where: {
+      userId: (session.user as User).id,
+      date: {
+        gte: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+}
--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
-import { User } from "@prisma/client"
 import { format } from "date-fns"
 import { ptBR } from "date-fns/locale"
+import { getConfirmedBookings } from "./_data/get-confirmed-bookings"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -23,26 +23,7 @@ const Home = async () => {
     },
   })
 
-  const confirmedBookings = session?.user
-    ? await db.booking.findMany({
-        where: {
-          userId: (session.user as User).id,
-          date: {
-            gte: new Date(),
-          },
-        },
-        include: {
-          service: {
-            include: {
-              barbershop: true,
-            },
-          },
-        },
-        orderBy: {
-          date: "asc",
-        },
-      })
-    : []
+  const confirmedBookings = await getConfirmedBookings()
 
   return (
     <div>
